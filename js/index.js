9e41ffc4eeedfe6844eb0434f55757b3b9038c2b
Daniel Pencak <danielpencak@gmail.com>
2016-12-04 11:04:06 -0800

    ESlint cleanup

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 (function() {
   'use strict';
-
-  let computerCards = [];
-
   const $xhr = $.ajax({
     method: 'GET',
     url:
-    `https://deckofcardsapi.com/api/deck/new/draw/?count=5`,
+    'https://deckofcardsapi.com/api/deck/new/draw/?count=5',
     dataType: 'json'
   });
 
@@ -14,22 +11,24 @@
     if ($xhr.status !== 200) {
       return;
     }
-    const card = {};
-    console.log(data)
     console.log(data.remaining);
-    console.log(data.deck_id);
     const deckID = data.deck_id;
+
     console.log(deckID);
     console.log(data.cards.length);
     const playerCards = data.cards.map((playerCard) => {
       return {
         value: playerCard.value,
         image: playerCard.image
-      }
+      };
     });
+
     for (const playerCard of playerCards) {
       const $span = $('#userHand');
-      const $img = $('<img>').addClass('responsive-img playerCard').attr({alt: 'Card', src: `${playerCard.image}`});
+      const $img = $('<img>').addClass('responsive-img playerCard');
+
+      $img.attr({ alt: 'Card', src: `${playerCard.image}` });
+
       $span.append($img);
     }
     console.log(playerCards);
@@ -41,27 +40,28 @@
       dataType: 'json'
     });
 
-    $xhr2.done((data) => {
+    $xhr2.done((data2) => {
       if ($xhr2.status !== 200) {
         return;
       }
-      const card = {};
-      console.log(data)
-      console.log(data.remaining);
-      console.log(data.deck_id);
-      const deckID = data.deck_id;
+      console.log(data2.remaining);
+      console.log(data2.deck_id);
+
       console.log(deckID);
-      console.log(data.cards.length);
-      const computerCards = data.cards.map((computerCard) => {
+      console.log(data2.cards.length);
+      const computerCards = data2.cards.map((computerCard) => {
         return {
           value: computerCard.value,
           image: computerCard.image
-        }
+        };
       });
+
       console.log(computerCards);
       for (const computerCard of computerCards) {
         const $span = $('#computerHand');
-        const $img = $('<img>').addClass('responsive-img imgCardBack').attr({alt: 'Card', src: 'images/card_back.jpg'});
+        const $img = $('<img>').addClass('responsive-img imgCardBack');
+
+        $img.attr({ alt: 'Card', src: 'images/card_back.jpg' });
         $span.append($img);
       }
     });
